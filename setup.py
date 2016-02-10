3cd9c6e02abf865337d90f4fe68c2c616dc90127
Michael Moncada <michael.moncada@oicr.on.ca>
2016-02-10 13:11:58 -0500

    * Update setup.py to be more verbose (in preparation for distribution).

--- setup.py
+++ setup.py
@@ -5,13 +5,23 @@ from setuptools import setup
 setup(
     name='BSCodeTabs',
     version='1.0',
+    url='https://github.com/mikecules/MarkdownBSCodeTabs',
     py_modules=['BSCodeTabExtension'],
     install_requires = ['markdown>=2.6'],
     description='Converts Fenced Sequential Blocks in Markdown into Bootstrap HTML Tabs.',
     author='Michael Moncada',
     author_email='michael.moncada@gmail.com',
-    license='GPL3',
+    license='BSD',
     platforms='Operating System :: OS Independent',
     long_description="""This plugin will convert consecutive fences blocks and convert it to HTML Bootstrap Tabs.
-    PLEASE NOTE: this extension requires Bootstrap 3.x+ to work correctly the frontend/browser."""
+    PLEASE NOTE: this extension requires Bootstrap 3.x+ to work correctly the frontend/browser.""",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Text Processing'
+    ]
 )
